6a281318e5b28886a01842bfd6d13bd05d8cd90c
m-kovalsky <mikova@microsoft.com>
2021-04-25 08:54:16 +0300

    v1.0.1

--- SetIsAvailableInMDXProperty_MemorySavings.cs
+++ SetIsAvailableInMDXProperty_MemorySavings.cs
@@ -1,16 +1,18 @@
 var sb = new System.Text.StringBuilder(); 
-
-// Headers
-sb.Append("TableName" + '\t' + "ColumnName" + '\t' + "HierarchySize" );
-sb.Append(Environment.NewLine);
-
+string newline = Environment.NewLine;
 string ann = "Vertipaq_ColumnHierarchySize";
 long tot = 0;
+
+// Header
+sb.Append("TableName" + '\t' + "ColumnName" + '\t' + "HierarchySize" + newline);
+
 foreach (var c in Model.AllColumns.Where(a => a.IsAvailableInMDX && (a.IsHidden || a.Table.IsHidden) && ! a.UsedInSortBy.Any() && ! a.UsedInHierarchies.Any() ).OrderBy(a => a.Table.Name).ThenBy(a => a.Name))
 {
-    sb.Append(c.Table.Name + '\t' + c.Name + '\t' + c.GetAnnotation(ann));
-    tot = tot + Convert.ToInt64(c.GetAnnotation(ann));
-    sb.Append(Environment.NewLine);
+    string tableName = c.Table.Name;
+    string colName = c.Name;
+    string annValue = c.GetAnnotation(ann);
+    sb.Append(tableName + '\t' + colName + '\t' + annValue + newline);
+    tot = tot + Convert.ToInt64(annValue);
 }
 
 tot.Output(); // Value shown in bytes
