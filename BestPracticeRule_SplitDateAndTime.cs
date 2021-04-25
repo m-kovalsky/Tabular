500c073bb89c152f2bee794360235de4e25a1087
m-kovalsky <mikova@microsoft.com>
2021-04-25 08:47:02 +0300

    v1.0.1

--- BestPracticeRule_SplitDateAndTime.cs
+++ BestPracticeRule_SplitDateAndTime.cs
@@ -2,23 +2,20 @@
 string annName = "DateTimeWithHourMinSec";
 foreach (var c in Model.AllColumns.Where(a => a.DataType == DataType.DateTime))
 {
-    string columnName = c.Name;
-    string tableName = c.Table.Name;
-    var obj = Model.Tables[tableName].Columns[columnName];
-    
-    var result = ExecuteDax("EVALUATE TOPN(5,SUMMARIZECOLUMNS('"+tableName+"'["+columnName+"]))").Tables[0];
+    string objName = c.DaxObjectFullName;
+    var result = ExecuteDax("EVALUATE TOPN(5,SUMMARIZECOLUMNS("+objName+"))").Tables[0];
 
     for (int r = 0; r < result.Rows.Count; r++)
-	{
-	    string resultValue = result.Rows[r][0].ToString();
-	    if (!resultValue.EndsWith("12:00:00 AM"))
-	    {
-	        obj.SetAnnotation(annName,"Yes");
-	        r=50;
-	    }
-	    else
-	    {
-	        obj.SetAnnotation(annName,"No");
-	    }
-	}
+    {
+        string resultValue = result.Rows[r][0].ToString();
+        if (!resultValue.EndsWith("12:00:00 AM"))
+        {
+            c.SetAnnotation(annName,"Yes");
+            r=50;
+        }
+        else
+        {
+            c.SetAnnotation(annName,"No");
+        }
+    }
 }
