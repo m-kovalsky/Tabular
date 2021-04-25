64450ac1df906d0b367c01e8571445aff71194dd
m-kovalsky <mikova@microsoft.com>
2021-04-25 08:43:03 +0300

    v1.0.1

--- BestPracticeRule_LongLengthColumns.cs
+++ BestPracticeRule_LongLengthColumns.cs
@@ -3,16 +3,13 @@ string annName = "LongLengthRowCount";
 
 foreach (var c in Model.AllColumns.Where(a => a.DataType == DataType.String))
 {
-    string tableName = c.Table.Name;
-    string columnName = c.Name;
+    string objName = c.DaxObjectFullName;
+    var result = EvaluateDax("SUMMARIZECOLUMNS(\"test\",CALCULATE(COUNTROWS(DISTINCT("+objName+")),LEN("+objName+") > "+maxLen+"))");
     
-    var obj = Model.Tables[tableName].Columns[columnName];
-    var result = EvaluateDax("SUMMARIZECOLUMNS(\"test\",CALCULATE(COUNTROWS(DISTINCT('"+tableName+"'["+columnName+"])),LEN('"+tableName+"'["+columnName+"]) > "+maxLen+"))");
+    c.SetAnnotation(annName,result.ToString());
     
-    obj.SetAnnotation(annName,result.ToString());
-    
-    if (obj.GetAnnotation(annName) == "Table")
+    if (c.GetAnnotation(annName) == "Table")
     {
-        obj.SetAnnotation(annName,"0");
+        c.SetAnnotation(annName,"0");
     }
 }
