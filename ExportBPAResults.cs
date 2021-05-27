96ac8148840b1cf88ce92d90a29145953a3b5c11
m-kovalsky <mikova@microsoft.com>
2021-05-27 12:28:42 +0300

    v1.0.0

---
+++ ExportBPAResults.cs
@@ -0,0 +1,16 @@
+using TabularEditor.BestPracticeAnalyzer;
+
+var bpa = new Analyzer();
+bpa.SetModel(Model);
+
+var sb = new System.Text.StringBuilder();
+string newline = Environment.NewLine;
+
+sb.Append("RuleCategory" + '\t' + "RuleName" + '\t' + "ObjectName" + '\t' + "ObjectType" + '\t' + "RuleSeverity" + '\t' + "HasFixExpression" + newline);
+
+foreach (var a in bpa.AnalyzeAll().ToList())
+{
+    sb.Append(a.Rule.Category + '\t' + a.RuleName + '\t' + a.ObjectName + '\t' + a.ObjectType + '\t' + a.Rule.Severity + '\t' + a.CanFix + newline);
+}
+
+sb.Output();
